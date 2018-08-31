58b661823ac4d01416b3717c15f8c5e2e5fedbe1
Maxsuel Marinho <maxsuelmarinho@gmail.com>
2018-08-31 20:25:21 -0300

    Add basic authentication

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,11 @@
 class Comment < ApplicationRecord
   belongs_to :article
+
+  before_destroy :can_destroy?
+  
+  private
+
+  def can_destroy?
+    throw(:abort) unless %w(MAX).include? commenter
+  end
 end
